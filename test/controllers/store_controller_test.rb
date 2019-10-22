095e22209075a21118f987217d136906a48fd8b3
SG24 <shreyanshgupta98@gmail.com>
2019-10-22 12:45:07 +0530

    adds tests for view counter

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -9,4 +9,21 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
     assert_select 'h3', 'Programming Ruby 1.9'
     assert_select '.price', /\$[,\d]+\.\d\d/
   end
+
+  test 'should display views if greater than 4 and reset to 0 if new items are added to cart' do
+    get store_index_url
+    assert_select 'p#counter', text: '', count: 1
+    assert_equal 1, session[:counter]
+    5.times do
+      get store_index_url
+    end
+    assert_equal 6, session[:counter]
+    assert_select 'p#counter', text: '6 views', count: 1
+    # Adds a new line item to cart
+    post line_items_url(product_id: products(:one).id)
+    # Redirects to home page
+    get store_index_url
+    assert_equal 1, session[:counter]
+    assert_select 'p#counter', text: '', count: 1
+  end
 end
