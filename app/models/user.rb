f61c6d9bc9cd1f363d17075473f2d4bb4032a3a2
SG24 <shreyanshgupta98@gmail.com>
2019-10-24 17:41:34 +0530

    adds a transaction to prevent deletion of the last admin user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_secure_password
+
+  after_destroy :ensure_an_admin_remains
+
+  class Error < StandardError
+  end
+
+  private
+
+  def ensure_an_admin_remains
+    # to only abort the transaction and not otherwise signal an exception
+    # raise ActiveRecord::Rollback exception instead, it won't be passed
+    # on by ActiveRecord::Base.transaction
+    raise Error, "Can't delete last user" if User.count.zero?
+  end
 end
